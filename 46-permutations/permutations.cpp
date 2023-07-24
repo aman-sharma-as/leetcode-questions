c9cb6d2e4773bbdc88a62e0831a2b6a3cdc835eb
Aman Sharma <55404326+aman-sharma-as@users.noreply.github.com>
2023-07-24 13:29:01 +0530

    Update permutations.cpp

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,12 +2,15 @@ class Solution {
 private:
     void solve(vector<int> &nums, vector<vector<int>> &ans, int index){
         if(index >= nums.size()){
+            // Storing final output in answer
             ans.push_back(nums);
             return;
         }
         for(int i = index; i < nums.size(); i++){
+            // swapping for all iterations of elements
             swap(nums[index], nums[i]);
             solve(nums, ans, index + 1);
+            // swapping back to backtrack the original array to prevent inconsistent answers
             swap(nums[index], nums[i]);
         }
     }
