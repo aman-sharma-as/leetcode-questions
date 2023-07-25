61c631a8089e2e6fdf04bc31745769daa8eef141
Aman Sharma <55404326+aman-sharma-as@users.noreply.github.com>
2023-07-25 16:33:03 +0530

    Update number-of-good-pairs.cpp

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -9,5 +9,17 @@ public:
             m[num]++;
         }
         return ans;
+
+        //Using two for loops
+        // int count = 0;
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 };
