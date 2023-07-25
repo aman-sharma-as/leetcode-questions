c17e5696304be03a474ff937f83c8fd613497c71
Aman Sharma <55404326+aman-sharma-as@users.noreply.github.com>
2023-07-25 22:18:09 +0530

    Time: 3 ms (98.06%) | Memory: 10.9 MB (94.59%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -2,21 +2,34 @@
 class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
-        vector<int> ans;
         int n = nums.size();
-        int index = 0;
-        while(index < n){
-            int leftSum = 0, rightSum = 0;
-            for(int i = 0; i < index; i++){
-                leftSum += nums[i];
-            }
-            for(int i = index + 1; i < n; i++){
-                rightSum += nums[i];
-            }
-            int sum = abs(leftSum - rightSum);
-            ans.push_back(sum);
-            index++;
+        vector<int> ans(n,0);
+        int rightSum = accumulate(nums.begin()+1, nums.end(), 0);
+        ans[0] = rightSum;
+        int leftSum = nums[0];
+        for(int i = 1; i < n; i++){
+            rightSum -= nums[i];
+            ans[i] = abs(leftSum - rightSum);
+            leftSum +=nums[i];
         }
         return ans;
+        
+        
+        
+        //-------x------ Brute Force ----x-------
+        // int index = 0;
+        // while(index < n){
+        //     int leftSum = 0, rightSum = 0;
+        //     for(int i = 0; i < index; i++){
+        //         leftSum += nums[i];
+        //     }
+        //     for(int i = index + 1; i < n; i++){
+        //         rightSum += nums[i];
+        //     }
+        //     int sum = abs(leftSum - rightSum);
+        //     ans.push_back(sum);
+        //     index++;
+        // }
+        // return ans;
     }
 };
