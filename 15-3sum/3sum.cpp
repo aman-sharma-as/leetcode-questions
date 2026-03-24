93b024f6a2fddf42bd2d8665a2ce531cd5719321
Aman Sharma <55404326+aman-sharma-as@users.noreply.github.com>
2026-03-24 22:49:36 +0530

    Time: 49 ms (52.15%) | Memory: 29 MB (91.42%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // Declaring and assining variables
+        int mainPointer = 0, leftPointer, rightPointer, numsSize = nums.size(),
+        sum = 0;
+        vector<vector<int>> result;
+
+        // Sorting given array
+        sort(nums.begin(), nums.end());
+        for(;mainPointer < numsSize - 2; mainPointer++){
+            if(mainPointer > 0 && nums[mainPointer] == nums[mainPointer-1])
+                continue;
+            sum = -1 * nums[mainPointer];
+            leftPointer = mainPointer+1;
+            rightPointer = numsSize-1;
+            while(leftPointer < rightPointer){
+                if(sum == nums[leftPointer]+nums[rightPointer]){
+                    result.push_back({nums[mainPointer],nums[leftPointer],
+                    nums[rightPointer]});
+                    leftPointer++;
+                    rightPointer--;
+                    while(leftPointer < numsSize && 
+                    nums[leftPointer] == nums[leftPointer-1])
+                        leftPointer++;
+                    while(rightPointer > mainPointer && nums[rightPointer]== nums[rightPointer+1])
+                        rightPointer--;
+                }
+                else if(sum > nums[leftPointer]+nums[rightPointer]){
+                    leftPointer++;
+                }
+                else{
+                    rightPointer--;
+                }
+            }
+        }
+        return result;
+    }
+};
